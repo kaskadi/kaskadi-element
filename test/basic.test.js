ba57b2c9b0b59dd442febf14ddee6add50c5678e
Holger Will <h.will@klimapartner.de>
2020-01-25 07:26:50 +0100

    strange bug does throw

--- test/basic.test.js
+++ test/basic.test.js
@@ -3,11 +3,11 @@ import './test-element.js'
 describe('kaskadi-element', () => {
   it('shou be usable as base class for custom elements', async () => {
     // STRANGE BUG!!! MUST INVESTIGATE!!!
-    try {
-      document.createElement('test-element')
-    } catch (err) {
-      console.log('hi')
-    }
+    // try {
+    //   document.createElement('test-element')
+    // } catch (err) {
+    //   console.log('hi')
+    // }
     document.body.innerHTML = '<test-element></test-element>'
     const elem = document.querySelector('test-element')
     await elem.updateComplete
